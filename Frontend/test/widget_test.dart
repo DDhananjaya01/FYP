823c00ea31b269fcac68f03e2b49385a1870a527
DDhananjaya01 <desithadananjaya@gmail.com>
2026-02-03 13:05:09 +0530

    Revert "change files"
    This reverts commit 02df0a35448d841cf45b8ab81efc6403affc8efc.

--- Frontend/test/widget_test.dart
+++ Frontend/test/widget_test.dart
@@ -8,14 +8,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-void main() {
-  testWidgets('App loads smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester
-        .pumpWidget(const MaterialApp(home: Scaffold(body: Text('Test'))));
+import 'package:fyp_frontend/main.dart';
 
-    // Verify that something is rendered.
-    expect(find.text('Test'), findsOneWidget);
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
