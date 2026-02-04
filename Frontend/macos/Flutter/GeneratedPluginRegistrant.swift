895c2bab1b51ca55a4f902cc4c47ab447b27fc2d
DDhananjaya01 <desithadananjaya@gmail.com>
2026-02-05 01:52:31 +0530

    solve errors

--- Frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import audioplayers_darwin
+import battery_plus
+import device_info_plus
+import flutter_tts
 import path_provider_foundation
+import record_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  BatteryPlusMacosPlugin.register(with: registry.registrar(forPlugin: "BatteryPlusMacosPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
 }
